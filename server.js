10ac02fd5e6d77b14d3fee049b50ead7735e59c6
shiyuegongsui <simply_love12@163.com>
2021-01-25 17:50:33 +0800

    修改成同步打包

--- server.js
+++ server.js
@@ -5,21 +5,23 @@ const fs = require('fs');
 
 //监听的文件
 console.log('我开始自动编译SCSS文件');
+let timer = null;
 chokidar.watch('**/*.scss', {
 }).on('change', (file) => {
-  const { dir, name } = path.parse(file);
-  sass.render({
-    file: file
-  }, function (err, result) {
-    if (!err) {
-      const newFile = `${dir}/${name}.wxss`;
-      fs.writeFile(newFile, result.css, function (err) {
-        if (!err) {
-          console.log(`updated ${newFile}`);
-        }
-      });
-    }
-  });
+  timer && clearTimeout(timer);
+  timer = setTimeout(() => {
+    const { dir, name } = path.parse(file);
+    sass.render({
+      file: file
+    }, function (err, result) {
+      if (!err) {
+        const newFile = `${dir}/${name}.wxss`;
+        fs.writeFileSync(newFile, result.css);
+        console.log(`updated ${newFile}  at: ${new Date().toLocaleTimeString()}`);
+      }
+    });
+  }, 200)
+
 });
 
 
